389aef59cdf966c61a2394e5f7d117cac0d063b4
nervylof <85636723+nervylof@users.noreply.github.com>
2021-06-11 22:49:18 +0300

    Add files via upload

---
+++ Урок 3/Задание3.py
@@ -0,0 +1,7 @@
+print('Как вас зовут?')
+name = input()
+print('Как ваша фамилия?')
+surname = input()
+print('Как ваше отчество?')
+patronymic = input()
+print('Здравствуйте,', surname, name, patronymic)
