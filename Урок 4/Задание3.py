6575e22dab047864ef6b24e336ae5ad24e2fc640
nervylof <85636723+nervylof@users.noreply.github.com>
2021-06-12 03:48:09 +0300

    Add files via upload

---
+++ Урок 4/Задание3.py
@@ -0,0 +1,6 @@
+a = 123
+first = a // 100
+second = a % 100 // 10
+third = a % 10
+print('Сумма цифр числа', a, 'равна', first + second + third)
+print('Произведение цифр числа', a, 'равно', first * second * third)
