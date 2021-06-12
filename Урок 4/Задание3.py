0effc77aff2ca42a0a2aed4565817bea9ca85786
nervylof <85636723+nervylof@users.noreply.github.com>
2021-06-12 17:47:52 +0300

    Add files via upload

---
+++ Урок 4/Задание3.py
@@ -0,0 +1,6 @@
+num = int(input())
+first = (num // 100)
+second = (num % 100 // 10)
+third = (num % 10)
+print('Сумма цифр числа', num, 'равна', first + second + third)
+print('Произведение цифр числа', num, 'равна', first * second * third)
