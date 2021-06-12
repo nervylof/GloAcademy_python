befe7e4f7f380f0ca0f96b97ad5e6052cd20fb78
nervylof <85636723+nervylof@users.noreply.github.com>
2021-06-12 17:36:40 +0300

    Delete Задание3.1.py

--- Урок 4/Задание3.1.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-first = num // 100
-second = num % 100 // 10
-third = num % 10
-print('Сумма цифр числа', num, 'равна', first + second + third)
-print('Произведение цифр числа', num, 'равно', first * second * third)
