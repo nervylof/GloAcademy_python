1677563e09593b439b419b1a126026fe303237e9
nervylof <85636723+nervylof@users.noreply.github.com>
2021-06-14 20:36:31 +0300

    Add files via upload

---
+++ Урок5/Задание1.py
@@ -0,0 +1,11 @@
+print('Введите пароль')
+password_1 = input()
+print('Повторите пароль')
+password_2 = input()
+
+if password_1 == password_2:
+  print('Все окей!')
+
+else:
+  password_1 != password_2
+  print('Пароли не совпадают!')
