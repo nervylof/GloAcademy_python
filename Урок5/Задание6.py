f990f719de9fca0150ed672a2c7c1d7414d14501
nervylof <85636723+nervylof@users.noreply.github.com>
2021-06-15 18:48:56 +0300

    Add files via upload

---
+++ Урок5/Задание6.py
@@ -0,0 +1,20 @@
+num = int(input())
+if num < 0 or num > 36:
+  print('Ошибка ввода!')
+elif num == 0:
+  print('ZERO!')
+elif num > 0 and num < 11 and num % 2 == 0:
+  print('BLACK!')
+elif num > 18 and num < 29 and num % 2 == 0:
+  print('BLACK!')
+elif num > 10 and num < 19 and num % 2 > 0:
+  print ('BLACK!')
+elif num > 28 and num < 37 and num % 2 >0:
+  print('BLACK!')
+else:
+  print('READ!')
+
+
+
+
+
